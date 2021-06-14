86e821082d306271a9b3a7a467fae701d4277a11
Nicky Meuleman <nicky.dev@outlook.com>
2021-06-14 17:36:25 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-mdx-math-example",
+  },
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
